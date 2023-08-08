2d6b2642eb83172930b6f4f4df0c10939737eebb
Nachiket <jadhao1044@gmail.com>
2023-08-08 10:11:50 +0530

    first commit

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -11,7 +11,14 @@ public class OrderService {
     HashMap<String, List<String>> deliveryIdOrderListMap;
     HashMap<String,String> orderIdDeliveryIdMap;
 
+    public void method(){
+        System.out.println(idOrderMap);
+        System.out.println(idDeliveryPartnerMap);
+        System.out.println(deliveryIdOrderListMap);
+        System.out.println(orderIdDeliveryIdMap);
+    }
     public OrderService(){
+        method();
         idOrderMap = new HashMap<>();
         idDeliveryPartnerMap = new HashMap<>();
         deliveryIdOrderListMap = new HashMap<>();
@@ -19,54 +26,71 @@ public class OrderService {
     }
 
     public void addOrder(Order order){
+        method();
         String id = order.getId();
         idOrderMap.put(id,order);
     }
 
     public void addPartner(String id){
+        method();
         DeliveryPartner deliveryPartner = new DeliveryPartner(id);
         idDeliveryPartnerMap.put(id,deliveryPartner);
     }
 
     public void addOrderPartnerPair(String orderId,String deliverPartnerId){
+        method();
         orderIdDeliveryIdMap.put(orderId,deliverPartnerId);
         if(deliveryIdOrderListMap.containsKey(deliverPartnerId)){
-            deliveryIdOrderListMap.get(deliverPartnerId).add(orderId);
+            if(!deliveryIdOrderListMap.get(deliverPartnerId).contains(orderId)){
+                deliveryIdOrderListMap.get(deliverPartnerId).add(orderId);
+                DeliveryPartner deliveryPartner = idDeliveryPartnerMap.get(deliverPartnerId);
+                deliveryPartner.setNumberOfOrders(deliveryPartner.getNumberOfOrders()+1);
+            }
         }
         else{
             deliveryIdOrderListMap.put(deliverPartnerId,new ArrayList<>(){{add(orderId);}});
+            DeliveryPartner deliveryPartner = idDeliveryPartnerMap.get(deliverPartnerId);
+            deliveryPartner.setNumberOfOrders(deliveryPartner.getNumberOfOrders()+1);
         }
-        DeliveryPartner deliveryPartner = idDeliveryPartnerMap.get(deliverPartnerId);
-        deliveryPartner.setNumberOfOrders(deliveryPartner.getNumberOfOrders()+1);
+
     }
 
     public Order getOrderById(String id){
+        method();
         return idOrderMap.get(id);
     }
 
     public DeliveryPartner getPartnerById(String id){
+
+        method();
         return idDeliveryPartnerMap.get(id);
     }
 
     public Integer getOrderCountByPartnerId(String id){
+        method();
         return idDeliveryPartnerMap.get(id).getNumberOfOrders();
     }
 
         public List<String> getOrdersByPartnerId(String id){
+
+        method();
         return deliveryIdOrderListMap.get(id);
     }
 
     public List<String> getAllOrders() {
+        method();
         return new ArrayList<>(idOrderMap.keySet());
     }
 
     public Integer getCountOfUnassignedOrders(){
+        method();
         Integer total = idOrderMap.size();
         Integer p = orderIdDeliveryIdMap.size();
         return total-p;
     }
 
     public Integer getOrdersLeftAfterGivenTimeByPartnerId(String p,String partnerId){
+        method();
         int deliveryTime = 60*(Integer.parseInt(p.substring(0,2)))+(Integer.parseInt(p.substring(3,5)));
         List<String> orderId = deliveryIdOrderListMap.get(partnerId);
         int k = 0;
